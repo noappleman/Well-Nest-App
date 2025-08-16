c7c607d6a97e2deed0107c6bf169f765374c6ff4
noappleman <ngisaac15@gmail.com>
2025-08-17 00:29:10 +0800

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --worker-tmp-dir /dev/shm --workers=4 --threads=2 --worker-class=gthread app:app
---
+++ gunicorn_config.py
@@ -0,0 +1,5 @@
+workers = 4
+threads = 2
+worker_class = 'gthread'
+timeout = 120
+bind = '0.0.0.0:' + str(int(10000))
